01f9ecfde5cc0551f5b810a231c91aacb5639c57
raulkm108 <raulkm.civil@gmail.com>
2025-06-23 17:34:45 -0300

    Add_new_contact function
    Creating the add_new_contact function asking for the input for name, number, email and is the user wanna add it to the favorite list right away

--- main.py
+++ main.py
@@ -2,17 +2,37 @@ import functions
 
 contacts = []
 
+def add_new_contact(contacts):
+    name = input("Type the person's name: ")
+    number = input("Type it's phone number: ")
+    email = input("Type it's Email: ")
+    favorite_choice = input ("Do you want it to be in your favorite list? [y / n]")
+    while True:
+        if favorite_choice == "y":
+            favorite = True
+            break
+        elif favorite_choice == "n":
+            favorite = False
+            break
+        else
+            print("Type y or n: ")
+
+
 while True:
     print("Contact List.")
 
     print("1. Add a new contact")
     print("2. See contacts")
     print("3. Update contact")
-    print("4. Favorite a contact")
+    print("4. Set a contact to Favorite")
     print("5. See favorites")
     print("6. Erase a contact")
     print("7. Leave")
 
     choice = input("Choose an action: ")
-    if choice == "7":
+
+    if choice == "1":
+        add_new_contact(contacts)
+
+    elif choice == "7":
         break
